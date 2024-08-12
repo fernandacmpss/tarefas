fe9ca457c08ba15c26926f5c3ff088959b8dfa49
fernandacmpss <campos.fernanda@escolar.ifrn.edu.br>
2024-08-12 16:15:35 -0300

    fernanda

---
+++ algoritmos 2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Console.WriteLine("Digite seu nome");
+string nome = Console.ReadLine();
+Console.WriteLine("Olá, " + nome + ", digite o valor de a:");
+
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite agora o valor de b:");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("A soma de a e b é: " + (a + b)); 
+*/
+
+Console.WriteLine("Digite um numero:");
+int num = int.Parse(Console.ReadLine());
+bool par = num % 2 == 0;
+
+if(par)
+{
+    Console.WriteLine("é par");
+}    
+else
+{
+     Console.WriteLine("é impar");
+}
